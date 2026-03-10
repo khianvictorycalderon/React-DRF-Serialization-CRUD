39a766846504a2f3283f1b8176121a9c2e142502
khianvictorycalderon <ckhianvictory@gmail.com>
2026-03-10 12:53:46 +0800

    Added user model uuid

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
 
 class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     age = models.PositiveBigIntegerField()
     address = models.CharField(max_length=255)
