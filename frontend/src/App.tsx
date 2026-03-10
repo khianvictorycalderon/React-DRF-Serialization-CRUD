f01a54f06c33e049487535d26cedb27c6ff9b6d9
khianvictorycalderon <ckhianvictory@gmail.com>
2026-03-10 09:16:10 +0800

    Initial Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { ENV } from "./constants"
+
+export default function App() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+
+      <div className="text-center">
+        <h2 className="text-2xl font-bold">React + Django (REST Framework) + Serialization CRUD</h2>
+        <p>Content here...</p>
+      </div>
+
+    </div>
+  )
+}
