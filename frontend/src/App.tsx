3dc88de913357bd3ed0bf989a0d129e481ec29a9
khianvictorycalderon <ckhianvictory@gmail.com>
2026-03-10 13:59:50 +0800

    Removed type

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState, type FormEvent } from "react";
+import { useEffect, useState } from "react";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
